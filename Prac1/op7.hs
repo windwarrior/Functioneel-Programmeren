863db7cd7a8ed0309dd820217821032c024ed2a5
Lennart Buit <lennartbuit@gmail.com>
2012-09-07 08:54:56 +0200

    things

---
+++ Prac1/op7.hs
@@ -0,0 +1,16 @@
+import FPPrac
+import Data.Int
+
+-- De practicumhandleiding noemt hem 'r' dat vond ik onhandig
+rlist :: Number -> Number -> [Number]
+rlist s v = s : rlist (s+v) v
+
+-- De practicumhandleiding noemt hem 'r1' dat vond ik onhandig
+-- opzich kan dit met head, of met de !! operator, dan is het drop deel niet nodig
+getn :: Number -> Number -> Number -> Number
+getn s v i = head (drop (i - 1) (take i (rlist s v)))
+
+-- Hier heb ik gebruik gemaakt van de ingebouwde sum methode, omdat ik in opgave 6 al bewezen heb
+-- een lijst te kunnen optellen
+getsum :: Number -> Number -> Number -> Number -> Number
+getsum s v i j = sum( drop (i) (take j (rlist s v)))
