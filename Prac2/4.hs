d329f80746b51e8d4b0efcc05a0d5dae75303a73
Peter van Dijk <petervandijk1991@gmail.com>
2012-09-08 12:30:03 +0200

    opg 4

---
+++ Prac2/4.hs
@@ -0,0 +1,10 @@
+import FPPrac
+import Prelude (Bool)
+import Prelude (Int)
+
+isPyth :: (Int,Int,Int) -> Bool
+isPyth (a,b,c) = a^2 + b^2 == c^2
+
+--pyth :: Int -> [(Int,Int,Int)]
+pyth n = filter isPyth [(a,b,c) | a <- [0..n], b <- [0..n], c <- [0..n] ]
+pyth' n = filter isPyth [(a,b,c) | a <- [0..n], b <- [a..n], c <- [0..n] ]
