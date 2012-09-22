e587b0a13f2b5f18200e61e589d73fec1e822643
Peter van Dijk <petervandijk1991@gmail.com>
2012-09-22 23:58:30 +0200

    added examples

--- Prac4/TypesBinTree.hs
+++ Prac4/TypesBinTree.hs
@@ -20,3 +20,23 @@ pp :: Show a => Show b => BinTree a b -> RoseTree
 pp (BinLeaf val) = RoseNode (show val) []
 pp (BinNode val node1 node2) = RoseNode (show val) [pp node1, pp node2]
 
+example1a = BinNode 12
+	(BinLeaf 2)
+	(BinNode 34
+		(BinLeaf 82)
+		(BinLeaf 53)
+	)
+
+example1b = BinNode (12,54)
+	(BinLeaf (2,9))
+	(BinNode (34,1)
+		(BinLeaf (82,7))
+		(BinLeaf (53,1))
+	)
+
+example1c = BinNode 12
+	(BinLeaf U)
+	(BinNode 34
+		(BinLeaf U)
+		(BinLeaf U)
+	)
