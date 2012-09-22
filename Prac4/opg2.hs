942472abb8903329d49a24ab4ea6cecc750c9b67
Lennart Buit <lennartbuit@gmail.com>
2012-09-22 23:34:32 +0200

    Opgaveserie 4 bijna af

--- Prac4/opg2.hs
+++ Prac4/opg2.hs
@@ -6,6 +6,7 @@ import Data.Char (isNumber)
 import Data.Either
 data Types = E | G | O | H
 data Operator = Plus | Min | Multiply | Divide | Power
+	deriving (Show, Eq)
 type EitherNumberChar = Either Number Char
 parseOperator :: Char -> Operator -- (BinTree Operator Number, String)
 parseOperator x
@@ -30,12 +31,12 @@ parseExp E (x:xs)
     where
         (t1, r1) = parseExp E xs
         (BinNode oper _ _ , r2) = parseExp O r1
-        (BinNode _ _ t2, r3) = parseExp E r2
-        (BinNode _ _ _, r4) = parseExp H r3
+        (t2, r3) = parseExp E r2
+        (_, r4) = parseExp H r3
 
 parseExp O (x:xs) = (BinNode (parseOperator x) (BinLeaf 0) (BinLeaf 0), xs)
 
-parseExp G (x:xs) = (BinNode Plus (BinLeaf (atoi (x:""))) (BinLeaf 0), xs)
+parseExp G (x:xs) = (BinLeaf (atoi (x:"")), xs)
 
 parseExp H (x:xs) = (BinNode Plus (BinLeaf 0) (BinLeaf 0), xs)
 
@@ -45,12 +46,12 @@ parseExpVar :: Types -> String -> (BinTree Operator EitherNumberChar, String)
 parseExpVar E (x:xs)
     {-| x == ')' = -- Einde expressie-}
     | x == '(' = (BinNode oper t1 t2 , r4)
-    | isNumber x || x `elem` ['a'..'Z'] = parseExpVar G (x:xs)
+    | isNumber x || x `elem` ['a'..'z'] || x `elem` ['A'..'Z'] = parseExpVar G (x:xs)
     | otherwise = error "dit werkt niet"
     where
         (t1, r1) = parseExpVar E xs
         (BinNode oper _ _ , r2) = parseExpVar O r1
-        (BinNode _ _ t2, r3) = parseExpVar E r2
+        (t2, r3) = parseExpVar E r2
         (BinNode _ _ _, r4) = parseExpVar H r3
 
 parseExpVar O (x:xs) = (BinNode (parseOperator x) (BinLeaf (Left 0)) (BinLeaf (Left 0)), xs)
@@ -60,3 +61,72 @@ parseExpVar G (x:xs)
     | otherwise = (BinNode Plus (BinLeaf (Right x)) (BinLeaf (Left 0)), xs)
 
 parseExpVar H (x:xs) = (BinNode Plus (BinLeaf (Left 0)) (BinLeaf (Left 0)), xs)
+
+-- ---------------------------------------------------------------------------
+type EitherNumberString = Either Number String
+parseExpSpaceMulti :: Types -> String -> (BinTree Operator EitherNumberString, String)
+parseExpSpaceMulti E (x:xs)
+    {-| x == ')' = -- Einde expressie-}
+    | x == '(' = (BinNode oper t1 t2 , r4)
+	| x == ' ' = parseExpSpaceMulti E xs
+    | isNumber x || x `elem` ['a'..'z'] || x `elem` ['A'..'Z'] = parseExpSpaceMulti G (x:xs)
+    | otherwise = error "dit werkt niet"
+    where
+        (t1, r1) = parseExpSpaceMulti E xs
+        (BinNode oper _ _ , r2) = parseExpSpaceMulti O r1
+        (t2, r3) = parseExpSpaceMulti E r2
+        (BinNode _ _ _, r4) = parseExpSpaceMulti H r3
+
+parseExpSpaceMulti O (x:xs)
+	| x == ' ' = parseExpSpaceMulti O xs
+	| otherwise = (BinNode (parseOperator x) (BinLeaf (Left 0)) (BinLeaf (Left 0)), xs)
+
+parseExpSpaceMulti G (x:xs) 
+	| x == ' ' = parseExpSpaceMulti G xs
+    | isNumber x = (BinLeaf (Left (atoi (number))), (drop (length number) (x:xs)))
+    | x `elem` ['a'..'z'] || x `elem` ['A'..'Z'] = (BinLeaf (Right number), (drop (length number) (x:xs)))
+	| otherwise = error "dit is stuk"
+		where 
+			number = (parseNumber (x:xs) "")
+			
+parseExpSpaceMulti H (x:xs) 
+	| x == ' ' = parseExpSpaceMulti H xs
+	| otherwise = (BinNode Plus (BinLeaf (Left 0)) (BinLeaf (Left 0)), xs)
+
+data State = Number | Variable | Other
+	deriving (Show, Eq)
+
+-- Parsebare string -> RestString -> (ummerString -> RestString)
+-- Ik weet niet of het mag met zo'n hulpfunctie, maar het werkt :>
+parseNumber :: String -> String -> String
+parseNumber "" y = y
+parseNumber (x) (y)
+	| (numberFSM Number x) == Other && (numberFSM Variable x) == Other  = parseNumber (take (length x - 1) x) ((drop (length x - 1) x) ++ y)
+	| (numberFSM Number x) == Number = (x)
+	| (numberFSM Variable x) == Variable = (x)
+
+numberFSM :: State -> String -> State
+numberFSM i [] = i
+numberFSM Variable (x:xs)
+	| x  `elem` ['a'..'z'] || x `elem` ['A'..'Z'] = numberFSM Variable xs
+	| otherwise = numberFSM Other xs
+numberFSM Number (x:xs) 
+	| isNumber x  = numberFSM Number xs
+	| otherwise = numberFSM Other xs
+numberFSM _ (x:xs) = numberFSM Other xs
+
+-- ---------------------------------------------------------------------------
+
+evalExp1 :: String -> Number
+evalExp1 x = evalTree (fst (parseExp E x))
+
+evalTree :: BinTree Operator Number -> Number
+evalTree (BinLeaf x) = x
+evalTree (BinNode operator child1 child2)
+	| operator == Plus     = (evalTree (child1)) + (evalTree (child2))
+	| operator == Min      = (evalTree (child1)) - (evalTree (child2))
+	| operator == Multiply = (evalTree (child1)) * (evalTree (child2))
+	| operator == Divide   = (evalTree (child1)) / (evalTree (child2))
+	| operator == Power    = (evalTree (child1)) ^ (evalTree (child2))
+	
+assign :: Char -> Number
