072a24e88563d6ff28521b2e2aa24e23d9c79e00
Lennart Buit <lennartbuit@gmail.com>
2012-09-25 20:43:45 +0200

    Datatype af

--- Prac5/RBtree.hs
+++ Prac5/RBtree.hs
@@ -3,4 +3,4 @@ module RBtree where
 import FPPrac
 
 data My_Color = Red | Black
-data My_RBTree = My_RBLeaf Color | My_RBNode Color Number RBTree RBTree
+data My_RBTree = My_RBLeaf My_Color | My_RBNode My_Color Number My_RBTree My_RBTree
