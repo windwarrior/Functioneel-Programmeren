10b158386c4e2229a1bceeab932dc1130594e22d
Peter van Dijk <petervandijk1991@gmail.com>
2012-09-28 11:52:37 +0200

    greyColourFlip

--- Prac5/RBtree.hs
+++ Prac5/RBtree.hs
@@ -2,7 +2,7 @@
 module RBtree where
 import FPPrac
 
-data My_Color = Red | Black
+data My_Color = Red | Black | Grey
 	deriving (Show)
 data My_RBTree = My_RBLeaf My_Color | My_RBNode My_Color Number My_RBTree My_RBTree
 	deriving (Show)
--- Prac5/peter_delete.hs
+++ Prac5/peter_delete.hs
@@ -6,7 +6,86 @@ leftmostValue (My_RBNode _ _ (My_RBNode _ child _) child) = leftmostValue child
 
 removeLeftmostNode ::  My_RBTree ->  My_RBTree
 removeLeftmostNode (My_RBNode c1 n1 (My_RBNode c2 n2 ch1 ch2) ch3) = (My_RBNode c1 n1 (removeLeftmostNode (My_RBNode c2 n2 ch1 ch2)) ch3)
-removeLeftmostNode (My_RBNode Red n1 (My_RBLeaf Black) (My_RBLeaf Black) = My_RBLeaf Red
-removeLeftmostNode (My_RBNode Black n1 (My_RBLeaf Red) (My_RBLeaf Red) = My_RBLeaf Grey
-removeLeftmostNode (My_RBNode Red n1 (My_RBLeaf Black) (My_RBNode Black n2 c1 c2) = (My_RBNode Red n2 c1 c2)
-removeLeftmostNode (My_RBNode Black n1 (My_RBLeaf Red) (My_RBNode Red n2 c1 c2) = (My_RBNode Black n2 c1 c2)
+removeLeftmostNode (My_RBNode Red n1 (My_RBLeaf _) (My_RBLeaf _) = My_RBLeaf Red
+removeLeftmostNode (My_RBNode Black n1 (My_RBLeaf _) (My_RBLeaf _) = My_RBLeaf Grey
+removeLeftmostNode (My_RBNode Red n1 (My_RBLeaf _) (My_RBNode _ n2 c1 c2) = (My_RBNode Red n2 c1 c2)
+removeLeftmostNode (My_RBNode Black n1 (My_RBLeaf _) (My_RBNode _ n2 c1 c2) = (My_RBNode Black n2 c1 c2)
+
+
+greyColourFlip :: My_RBTree ->  My_RBTree
+-- (a) all black
+greyColourFlip (My_RBNode Black p 
+		(My_RBLeaf Grey)
+		(My_RBNode Black s
+			(My_RBLeaf Black)
+			(My_RBLeaf Black)
+		)
+	) = (My_RBNode Grey p
+		(My_RBLeaf Black)
+		(My_RBNode Red s
+			(My_RBLeaf Black)
+			(My_RBLeaf Black)
+		)
+	)
+-- (b) l is red
+greyColourFlip (My_RBNode c1 p 
+		(My_RBLeaf Grey)
+		(My_RBNode Black s
+			(My_RBNode Red l 
+				(My_RBLeaf Black)
+				(My_RBLeaf Black)
+			)
+			r
+		)
+	) = (My_RBNode c1 l 
+		(My_RBNode Black p
+			(My_RBLeaf Black)
+			(My_RBLeaf Black)
+		)
+		(My_RBNode Black s
+				(My_RBLeaf Black)
+				r
+		)
+	)
+-- (c) p is red
+greyColourFlip (My_RBNode Red p 
+		(My_RBLeaf Grey)
+		(My_RBNode Black s
+			(My_RBLeaf Black)
+			r
+		)
+	) = (My_RBNode Black s
+		(My_RBNode Red p
+			(My_RBLeaf Black)
+			(My_RBLeaf Black)
+		)
+		r
+	)
+-- (d) r is red
+greyColourFlip (My_RBNode Black p 
+		(My_RBLeaf Grey)
+		(My_RBNode Black s
+			(My_RBLeaf Black)
+			(My_RBLeaf Red)
+		)
+	) = (My_RBNode Black s
+		(My_RBNode Black p
+			(My_RBLeaf Black)
+			(My_RBLeaf Black)
+		)
+		(My_RBLeaf Black)
+	)
+-- (e) s is red
+greyColourFlip (My_RBNode Black p 
+		(My_RBLeaf Grey)
+		(My_RBNode Red s
+			(My_RBLeaf Black)
+			(My_RBLeaf Black)
+		)
+	) = (My_RBNode Black s
+		(My_RBNode Red p
+			(My_RBLeaf Grey)
+			(My_RBLeaf Black)
+		)
+		(My_RBLeaf Black)
+	)
