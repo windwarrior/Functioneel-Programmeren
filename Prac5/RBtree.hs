09983abacfc17b6f751d82514648f23307f80c4b
Lennart Buit <lennartbuit@gmail.com>
2012-09-28 02:39:39 +0200

    wat functies

--- Prac5/RBtree.hs
+++ Prac5/RBtree.hs
@@ -24,4 +24,113 @@ rootToBlack :: My_RBTree -> My_RBTree
 rootToBlack (My_RBLeaf color) = (My_RBLeaf Black) 
 rootToBlack (My_RBNode color num child1 child2) = (My_RBNode Black num child1 child2) 
 
+{-
+      N
+     /\
+    /  \
+   A    B
+  /
+ / 
+C
+-}
 colourFlip :: My_RBTree -> My_RBTree
+
+--C zit aan node A child 1
+colourFlip (My_RBNode Black n (My_RBNode Red a (My_RBNode Red c chc1 chc2) cha2) (My_RBNode Red b chb1 chb2)) = My_RBNode Red n (My_RBNode Black a (My_RBNode Red c chc1 chc2) cha2) (My_RBNode Black b chb1 chb2))
+
+--C zit aan node A child 2
+colourFlip (My_RBNode Black n (My_RBNode Red a cha1 (My_RBNode Red c chc1 chc2)) (My_RBNode Red b chb1 chb2)) = My_RBNode Red n (My_RBNode Black a cha1 (My_RBNode Red c chc1 chc2)) (My_RBNode Black b chb1 chb2))
+
+--C zit aan node B child 1
+colourFlip (My_RBNode Black n (My_RBNode Red a cha1 cha2) (My_RBNode Red b (My_RBNode Red c chc1 chc2) chb2)) = (My_RBNode Red n (My_RBNode Black a cha1 cha2) (My_RBNode Black b (My_RBNode Red c chc1 chc2) chb2))
+
+--C zit aan node B child 2
+colourFlip (My_RBNode Black n (My_RBNode Red a cha1 cha2) (My_RBNode Red b chb1 (My_RBNode Red c chc1 chc2))) = (My_RBNode Red n (My_RBNode Black a cha1 cha2) (My_RBNode Black b chb1 (My_RBNode Red c chc1 chc2)))
+
+
+{-
+      N
+     /
+    /  
+   A  
+  /
+ /   
+C
+-}
+rebalance :: My_RBTree -> My_RBTree
+
+--C zit aan node A child 1
+rebalance =  My_RBNode Black n (
+                (My_RBNode Red a
+                    (My_RBNode Red c (My_RBLeaf _) (My_RBLeaf _))
+                    (My_RBLeaf _))
+                (My_RBLeaf _)) = 
+                    My_RBNode Black n (
+                        My_RBNode Red a (
+                            (My_RBLeaf Black)
+                            (My_RBLeaf Black) )
+                        My_RBNode Red c (
+                            (My_RBLeaf Black)
+                            (My_RBLeaf Black) )
+
+--C zit aan node A child 2
+rebalance =  My_RBNode Black n (
+                (My_RBNode Red a
+                    (My_RBLeaf _))
+                    (My_RBNode Red c (My_RBLeaf _) (My_RBLeaf _))
+                (My_RBLeaf _)) = 
+                    My_RBNode Black n (
+                        My_RBNode Red a (
+                            (My_RBLeaf Black)
+                            (My_RBLeaf Black) )
+                        My_RBNode Red c (
+                            (My_RBLeaf Black)
+                            (My_RBLeaf Black) )
+
+--C zit aan node B child 1
+rebalance =  My_RBNode Black n (
+                (My_RBLeaf _))
+                (My_RBNode Red a
+                    (My_RBNode Red c (My_RBLeaf _) (My_RBLeaf _))
+                    (My_RBLeaf _)) = 
+                        My_RBNode Black n (
+                            My_RBNode Red c (
+                                (My_RBLeaf Black)
+                                (My_RBLeaf Black) )
+                            My_RBNode Red a (
+                                (My_RBLeaf Black)
+                                (My_RBLeaf Black) )
+
+--C zit aan node B child 2
+rebalance =  My_RBNode Black n (
+                (My_RBLeaf _))
+                (My_RBNode Red a
+                    (My_RBLeaf _))  
+                    (My_RBNode Red c (My_RBLeaf _) (My_RBLeaf _)) =
+                        My_RBNode Black n (
+                            My_RBNode Red c (
+                                (My_RBLeaf Black)
+                                (My_RBLeaf Black) )
+                            My_RBNode Red a (
+                                (My_RBLeaf Black)
+                                (My_RBLeaf Black) )
+
+{-balancedInsert :: Number -> My_RBTree -> My_RBTree
+balancedInsert n tree 
+    |
+
+    where
+        inserted = insert n tree -}
+
+leftMostValue :: My_RBTree -> My_RBTree
+leftmostValue My_RBNode _ number (My_RBLeaf _) _ = number
+leftmostValue My_RBNode _ _ (My_RBNode _ child _) child = leftmostValue child
+
+removeLeftmostNode ::  My_RBTree ->  My_RBTree
+removeLeftmostNode (My_RBNode c1 n1 (My_RBNode c2 n2 ch1 ch2) ch3) = (My_RBNode c1 n1 (removeLeftmostNode (My_RBNode c2 n2 ch1 ch2)) ch3)
+removeLeftmostNode (My_RBNode Red n1 (My_RBLeaf Black) (My_RBLeaf Black) = My_RBLeaf Red
+removeLeftmostNode (My_RBNode Black n1 (My_RBLeaf Red) (My_RBLeaf Red) = My_RBLeaf Grey
+removeLeftmostNode (My_RBNode Red n1 (My_RBLeaf Black) (My_RBNode Black n2 c1 c2) = (My_RBNode Red n2 c1 c2)
+removeLeftmostNode (My_RBNode Black n1 (My_RBLeaf Red) (My_RBNode Red n2 c1 c2) = (My_RBNode Black n2 c1 c2)
+
+
