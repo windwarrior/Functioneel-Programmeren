9aca70381bfd65d7b7a3bbecee412ef3c71141d0
Lennart Buit <lennartbuit@gmail.com>
2012-10-08 00:57:29 +0200

    things

--- Sudoku/solver.hs
+++ Sudoku/solver.hs
@@ -1,13 +1,26 @@
-type Sudoku = [[Number]]
+
+import FPPrac
+import Examples 
+
+type Sudoku = [[Square]]
+type Square = [Number]
+type Block = [[Square]] -- Same as sudoku, different meaning
 
 pass :: Sudoku -> Sudoku
 pass sud = checkBlock $ checkColumn $ checkRow sud
 
 checkBlock :: Sudoku -> Sudoku
-checkBlock sud = error "unimplemented"
+checkBlock sud = error "unimplemented checkBlock"
 
 checkColumn :: Sudoku -> Sudoku
-checkColumn sud = error "unimplemented"
+checkColumn sud = checkRow $ getColumns sud
 
 checkRow :: Sudoku -> Sudoku
-checkRow sud = error "unimplemented"
+checkRow (x:xs) = error "unimplemented checkRow"
+
+getBlock :: Number -> Number -> Sudoku -> Block
+getBlock x y sud = error "unimplemented getBlock"
+
+-- The idea is to use one of the prac2 functions to rotate the matrix
+getColumns :: Sudoku -> Sudoku
+getColumns sud = error "unimplemented getColumns"
