30d15dccaf8eac75a2f0b2d602b5ea859fe8adf2
Lennart Buit <lennartbuit@gmail.com>
2012-10-09 01:37:30 +0200

    MOAR gui stuff

--- Sudoku/Graphics.hs
+++ Sudoku/Graphics.hs
@@ -1,12 +1,29 @@
 import FPPrac
 import FPPrac.Graphics
+import FPPrac.Events
 import Solver
-main :: IO ()
-main = graphicsout $ drawSudoku
+import Debug.Trace
+
+data Store = Store {
+    sudoku :: Sudoku
+}
 
 sudokuSize = 400
 halfSudokuSize = 0.5 * sudokuSize
-blockSize = sudokuSize / 9
+fieldSize = sudokuSize / 9
+blockSize = sudokuSize / 3
+
+main :: IO ()
+main = graphicsout $ drawSudoku
+
+startStore :: Store
+startStore = Store {
+    sudoku = exampleSudokuEasy
+    }
+
+processKey :: Store -> Input -> (Store,[Output])
+processKey store (KeyIn a) = trace (show a) (store, [])
+processKey store _ = (store,[])
 
 drawCircleAt :: Float -> Float -> Picture
 drawCircleAt x y = Pictures[ translate x y $ Color black $ circleSolid 10]
@@ -14,19 +31,28 @@ drawCircleAt x y = Pictures[ translate x y $ Color black $ circleSolid 10]
 drawLineFromTo :: (Float, Float) -> (Float, Float) -> Picture
 drawLineFromTo  (x1, y1) (x2, y2) = Color black $ Line [(x1, y1), (x2, y2)]
 
+drawBackgrounds :: [Picture]
+drawBackgrounds = (map drawSingleBackground [(-1,-1), (-1,1), (1,-1), (1,1), (0,0)])
+
+drawSingleBackground :: (Float, Float) -> Picture
+drawSingleBackground (xPos, yPos) = translate (xPos * blockSize) (yPos * blockSize) $ Color (greyN 0.9) $ rectangleSolid blockSize blockSize
 
 drawSudoku :: Picture
 drawSudoku = Pictures $
- (map (Line) [[(-halfSudokuSize + x * blockSize, halfSudokuSize), (-halfSudokuSize + x * blockSize, -halfSudokuSize)]| x <- [0..9]]) -- these are the vertical boardlines
- ++ (map (Line) [[(halfSudokuSize, -halfSudokuSize + x * blockSize), (-halfSudokuSize, -halfSudokuSize + x * blockSize)]| x <- [0..9]]) -- these are the horizontal boardlines
-  ++ (drawNumbers exampleSudokuEasy):[]
+    (drawBackgrounds)
+    ++ (map (Line) [[(-halfSudokuSize + x * fieldSize, halfSudokuSize), (-halfSudokuSize + x * fieldSize, -halfSudokuSize)]| x <- [0..9]]) -- these are the vertical boardlines
+    ++ (map (Line) [[(halfSudokuSize, -halfSudokuSize + x * fieldSize), (-halfSudokuSize, -halfSudokuSize + x * fieldSize)]| x <- [0..9]]) -- these are the horizontal boardlines
+    ++ (drawNumbers exampleSudokuEasy 0)
   
-drawNumbers :: Sudoku -> Picture
-drawNumbers (x:xs) = drawLine x (0,0)
+drawNumbers :: Sudoku -> Float -> [Picture]
+drawNumbers [] _ = []
+drawNumbers (x:xs) row = (drawLine x (0,row)) ++ (drawNumbers xs (row+1))
 
-drawLine :: [Square] -> (Float, Float) ->  Picture
-drawLine [] _ = Pictures $ []
-drawLine (x:xs) (col, row) = [Translate (-halfSudokuSize + (col + 0.5) * blockSize) (-halfSudokuSize + (row + 0.5) * blockSize) $ Scale 0.15 0.15 $ Text x]:(drawLine xs (col+1, row))
+drawLine :: [Square] -> (Float, Float) ->  [Picture]
+drawLine [] _ = []
+drawLine (x:xs) (col, row)
+    | FPPrac.length x == 1 = (Translate (-1 * halfSudokuSize + (col + 0.5) * fieldSize) (halfSudokuSize - (row + 0.5) * fieldSize) $ Scale 0.1 0.1 $ Text $ show (x FPPrac.!! 0)):[]  ++ (drawLine xs (col+1, row))
+    | otherwise = (drawLine xs (col+1, row))
 
 row1 = [[ ],[ ],[ ],[8],[ ],[4],[9],[ ],[3]]
 row2 = [[ ],[8],[1],[6],[3],[ ],[ ],[ ],[5]]
@@ -38,3 +64,9 @@ row7 = [[ ],[ ],[ ],[4],[ ],[ ],[2],[8],[ ]]
 row8 = [[4],[ ],[ ],[ ],[8],[2],[6],[1],[ ]]
 row9 = [[6],[ ],[8],[9],[ ],[1],[ ],[ ],[ ]]
 exampleSudokuEasy = [row1,row2,row3,row4,row5,row6,row7,row8,row9]
+
+
+doSudoku :: IO ()
+doSudoku = installEventHandler "sudoku" processKey startStore startPic 10
+    where 
+        startPic = drawSudoku
