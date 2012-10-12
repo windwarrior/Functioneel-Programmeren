6a0e092e1b98f849e3d96899198e51ce0ad7c6cd
Lennart Buit <lennartbuit@gmail.com>
2012-10-12 10:41:09 +0200

    ScreenClear

--- Sudoku/Graphics.hs
+++ Sudoku/Graphics.hs
@@ -36,13 +36,21 @@ processKey store (MouseDown (x,y)) | f /= Nothing = trace (show i) (store, [])
 		f = hitField (x,y)
 		Just i = f
 -- Debug regel, print ingedrukte toets, matcht op KeyIn's only
-processKey store@Store{sudoku = sudo} (KeyIn 'h') = (store', o)
-	where
-		store' = Store{sudoku = (hsCheck sudo)}
-		o = [DrawPicture $ drawSudoku store']
-
+processKey store (KeyIn any)
+	| any == 'h' = applyFunction (hsCheck) store
+	| any == 'v' = applyFunction (vsCheck) store
+	| any == 'n' = applyFunction (npCheck) store
+	| otherwise = (store, [])
+		
 -- Catch all case
 processKey store _ = (store,[])
+
+
+applyFunction :: (Sudoku -> Sudoku) -> Store -> (Store, [Output])
+applyFunction f store@Store{sudoku = sudo} = (store', o)
+	where
+		store' = Store{sudoku = (f sudo)}
+		o = [ScreenClear, DrawPicture $ drawSudoku store']
 
 -- Deze functie werkt, misschien even bepalen wat 0,0 is, bovenin of onderin :>
 -- Er staat nu een mooie hack om het om te draaien
