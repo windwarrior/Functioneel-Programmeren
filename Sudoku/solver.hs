a7f1c7e9688cf2b3b4f9e915f947a404f381f37c
Peter van Dijk <petervandijk1991@gmail.com>
2012-10-17 10:07:30 +0200

    fourspaced

--- Sudoku/solver.hs
+++ Sudoku/solver.hs
@@ -8,7 +8,7 @@ type Sudoku = [[Square]]
 type Square = [Int]
 type Block = [[Square]] -- Same as sudoku, different meaning
 
--------------------------------------------------------------------------------		
+-------------------------------------------------------------------------------        
 --                                        Getters                                           --
 -------------------------------------------------------------------------------
 
@@ -27,28 +27,28 @@ getBlockRow x sud y =  (take 3 (drop (3*x ) (sud !! y)))
 
 getSquareAt :: (Int, Int) -> Sudoku -> Square
 getSquareAt (x,y) sud = sqr
-	where
-		row = getRow y sud
-		sqr = row !! x
+    where
+        row = getRow y sud
+        sqr = row !! x
 
--------------------------------------------------------------------------------		
+-------------------------------------------------------------------------------        
 --                                        Setters                                           --
 -------------------------------------------------------------------------------
 
 setSquare :: (Int, Int) -> Square -> Sudoku -> Sudoku
 setSquare (x,y) sq sud = sudokuResult
-	where
-		row = getRow y sud
-		(rowStart, rowEnd) = splitAt x row
-		rowResult = rowStart ++ [sq] ++ (drop 1 rowEnd)
-		(columnsStart, columnsEnd) = splitAt y sud
-		sudokuResult = columnsStart ++ [rowResult] ++ (drop 1 columnsEnd)
-		
+    where
+        row = getRow y sud
+        (rowStart, rowEnd) = splitAt x row
+        rowResult = rowStart ++ [sq] ++ (drop 1 rowEnd)
+        (columnsStart, columnsEnd) = splitAt y sud
+        sudokuResult = columnsStart ++ [rowResult] ++ (drop 1 columnsEnd)
+        
 -- GUI to solver functions
 setSquareWithSafety :: (Int, Int) -> Int -> (Sudoku, Sudoku) -> (Sudoku, Bool)
 setSquareWithSafety (x,y) num (sud_unsolved, sud_solved)
-	| num `elem` (getSquareAt (x,y) sud_solved) = (setSquare (x,y) [num] sud_unsolved, True)
-	| otherwise = (sud_unsolved, False)
+    | num `elem` (getSquareAt (x,y) sud_solved) = (setSquare (x,y) [num] sud_unsolved, True)
+    | otherwise = (sud_unsolved, False)
 
     
 {-
@@ -73,15 +73,15 @@ setSmallSquareWithSafety (x,y,num) (sud, sud_solved)
 
 setBlock :: Block -> Int -> Int -> Sudoku -> Sudoku
 setBlock block x y sud = firstRows ++ newMiddleRows ++ lastRows
-	where
-		firstRows = take (3*x) sud
-		lastRows = drop (3*(x+1)) sud
-		middleRows = take 3 (drop (3*x) sud)
-		middleRowsBegin = map (take (3*y)) middleRows
-		middleRowsEnd = map (drop (3*(y+1))) middleRows
-		newMiddleRows = map (\x -> (middleRowsBegin !! x) ++ (block !! x) ++ (middleRowsEnd !! x)) [0..2]
+    where
+        firstRows = take (3*x) sud
+        lastRows = drop (3*(x+1)) sud
+        middleRows = take 3 (drop (3*x) sud)
+        middleRowsBegin = map (take (3*y)) middleRows
+        middleRowsEnd = map (drop (3*(y+1))) middleRows
+        newMiddleRows = map (\x -> (middleRowsBegin !! x) ++ (block !! x) ++ (middleRowsEnd !! x)) [0..2]
 
--------------------------------------------------------------------------------		
+-------------------------------------------------------------------------------        
 --                                     Convertors                                         --
 -------------------------------------------------------------------------------
 
@@ -91,9 +91,9 @@ blockToRow block = foldl1 (++) block
 
 rowToBlock :: [Square] -> Block
 rowToBlock row = [(fst rowBegin), (fst rowEnd), (snd rowEnd)]
-	where
-		rowBegin = splitAt 3 row
-		rowEnd = splitAt 3 (snd rowBegin)
+    where
+        rowBegin = splitAt 3 row
+        rowEnd = splitAt 3 (snd rowBegin)
 
 --Prepares sudoku for solver
 prep :: Sudoku -> Sudoku
@@ -102,8 +102,8 @@ prep sud = map prepRow sud
 prepRow :: [Square] -> [Square]
 prepRow [] = []
 prepRow (x:xs) 
-	| x == [] = [1..9] : (prepRow xs)
-	| otherwise = x : (prepRow xs)
+    | x == [] = [1..9] : (prepRow xs)
+    | otherwise = x : (prepRow xs)
 
 -- Subtracts x from y, keeping single values
 mudiff :: [Int] -> [Int] -> [Int]
@@ -122,18 +122,18 @@ getNumbers ([x]:xs) = x : (getNumbers xs)
 getNumbers (x:xs) = getNumbers xs
 getNumbers [] = []
 
--------------------------------------------------------------------------------		
+-------------------------------------------------------------------------------        
 --                                     Checkers                                           --
 -------------------------------------------------------------------------------
 
 --Checks sudoku recursively till no more changes are made by f
 recCheck :: (Sudoku -> Sudoku) -> Sudoku -> Sudoku
 recCheck f sud
-	| pass1 == pass2 = pass1
-	| otherwise = recCheck f pass2
-	where 
-		pass1 = f sud
-		pass2 = f pass1
+    | pass1 == pass2 = pass1
+    | otherwise = recCheck f pass2
+    where 
+        pass1 = f sud
+        pass2 = f pass1
 
 --Checks whole sudoku (columns, rows and blocks respectively) once with f
 checkSudoku :: ([Square] -> [Square]) -> Sudoku -> Sudoku
@@ -142,35 +142,35 @@ checkSudoku f sud = (checkBlocks f) (map f (getColumns (map f (getColumns sud)))
 -- Checks every block with f
 checkBlocks :: ([Square] -> [Square]) -> Sudoku -> Sudoku
 checkBlocks f sud = foldl checkBlockColumn sud [0..2]
-	where 
-		checkBlockColumn = (\sud y->(foldl (\sud x -> (setBlock (rowToBlock (f (blockToRow (getBlock x y sud)))) x y sud)) sud [0..2]))
+    where 
+        checkBlockColumn = (\sud y->(foldl (\sud x -> (setBlock (rowToBlock (f (blockToRow (getBlock x y sud)))) x y sud)) sud [0..2]))
 
 -- Solving algorithms, uses functions till result stays the same
 solve :: Sudoku -> Sudoku
 solve sud = solveWith [
-	(recCheck vsCheck), 
-	(recCheck hsCheck), 
-	(recCheck npCheck), 
-	(recCheck hpCheck)
-	] 0 (prep sud)
-	
+    (recCheck vsCheck), 
+    (recCheck hsCheck), 
+    (recCheck npCheck), 
+    (recCheck hpCheck)
+    ] 0 (prep sud)
+    
 solveWith :: [(Sudoku -> Sudoku)] -> Int -> Sudoku -> Sudoku
 solveWith functions i sud 
-	| i==n 					= sud --If hardest doesn't work return best effort
-	| checked /= sud 	= solveWith functions 0 checked --Use first algorithm after solving with harder one
-	| otherwise = solveWith functions (i+1) checked --If current algorithm doesn't work use harder one
-	where 
-		n = length functions
-		checked = (functions !! i) sud
-	
--------------------------------------------------------------------------------		
+    | i==n                     = sud --If hardest doesn't work return best effort
+    | checked /= sud     = solveWith functions 0 checked --Use first algorithm after solving with harder one
+    | otherwise = solveWith functions (i+1) checked --If current algorithm doesn't work use harder one
+    where 
+        n = length functions
+        checked = (functions !! i) sud
+    
+-------------------------------------------------------------------------------        
 --                        Visibles Singles Algorithm                                 --
 -------------------------------------------------------------------------------
 
 vsCheck :: Sudoku -> Sudoku
 vsCheck sud = checkSudoku (\row -> map (mudiff (getNumbers row)) row) sud
 
--------------------------------------------------------------------------------		
+-------------------------------------------------------------------------------        
 --                         Hidden Singles Algorithm                                 --
 -------------------------------------------------------------------------------
 
@@ -182,22 +182,22 @@ getHiddenSingles row = filter (\x -> length x == 1) (Data.List.group (Data.List.
 
 removeHiddenSingles :: [Square] -> [Square]
 removeHiddenSingles row 
-	| singles == [] = row
-	| otherwise = map (\sq -> filterHiddenSingles sq hs) row
-	where
-		singles = (getHiddenSingles row)
-		hs = foldl1 (++) singles
+    | singles == [] = row
+    | otherwise = map (\sq -> filterHiddenSingles sq hs) row
+    where
+        singles = (getHiddenSingles row)
+        hs = foldl1 (++) singles
 
 filterHiddenSingles :: Square -> [Int] -> Square
 filterHiddenSingles [x] _ = [x]
 filterHiddenSingles sq hs
-	| hasHiddenSingle = hiddenSingles
-	| otherwise = sq
-	where
-		hiddenSingles = (intersect hs sq)
-		hasHiddenSingle = not (hiddenSingles == [])
+    | hasHiddenSingle = hiddenSingles
+    | otherwise = sq
+    where
+        hiddenSingles = (intersect hs sq)
+        hasHiddenSingle = not (hiddenSingles == [])
 
--------------------------------------------------------------------------------		
+-------------------------------------------------------------------------------        
 --                             Naked Pair Algorithm                                  --
 -------------------------------------------------------------------------------
 
@@ -206,26 +206,26 @@ npCheck sud = checkSudoku (\row -> removeNakedPairs row) sud
 
 getNakedPairs :: [Square] -> [Square]
 getNakedPairs row 
-	| pairsOfpairs == [] = []
-	| otherwise = nub (foldl1 (++) pairsOfpairs)
-	where
-		pairs = (Data.List.group (Data.List.sort (filter (\x -> length x == 2) row)))
-		pairsOfpairs = (filter (\x -> length x == 2) pairs)
+    | pairsOfpairs == [] = []
+    | otherwise = nub (foldl1 (++) pairsOfpairs)
+    where
+        pairs = (Data.List.group (Data.List.sort (filter (\x -> length x == 2) row)))
+        pairsOfpairs = (filter (\x -> length x == 2) pairs)
 
 removeNakedPairs :: [Square] -> [Square]
 removeNakedPairs row 
-	| pairs == [] = row
-	| otherwise = map (\x -> filterNakedPairs x pairs numbers) row
-	where 
-		pairs = getNakedPairs row
-		numbers = nub (foldl1 (++) pairs)
+    | pairs == [] = row
+    | otherwise = map (\x -> filterNakedPairs x pairs numbers) row
+    where 
+        pairs = getNakedPairs row
+        numbers = nub (foldl1 (++) pairs)
 
 filterNakedPairs :: Square -> [Square] -> [Int] -> Square
 filterNakedPairs sq pairs numbers 
-	| sq `elem` pairs = sq
-	| otherwise = mudiff numbers sq
+    | sq `elem` pairs = sq
+    | otherwise = mudiff numbers sq
 
--------------------------------------------------------------------------------		
+-------------------------------------------------------------------------------        
 --                             Hidden Pair Algorithm                                 --
 -------------------------------------------------------------------------------
 
@@ -234,29 +234,29 @@ hpCheck sud = checkSudoku (\row -> removeHiddenPairs row (filterHiddenPairs row)
 
 getHiddenPairs :: [Square] -> [Int]
 getHiddenPairs row
-	| grouped == [] = []
-	| otherwise = duplicates
-	where
-		folded = foldl1 (++) row
-		grouped = filter (\x -> length x == 2) (Data.List.group (Data.List.sort folded))
-		duplicates = nub (foldl1 (++) grouped)
+    | grouped == [] = []
+    | otherwise = duplicates
+    where
+        folded = foldl1 (++) row
+        grouped = filter (\x -> length x == 2) (Data.List.group (Data.List.sort folded))
+        duplicates = nub (foldl1 (++) grouped)
 
 filterHiddenPairs :: [Square] -> ([Square], [Square])
 filterHiddenPairs row
-	| pairs /= [] = (intersection, result)
-	| otherwise = ([], [])	
-	where
-		possiblePairs = getHiddenPairs row
-		intersection = map (intersect possiblePairs) row
-		pairs = (filter (\x -> length x ==2) (Data.List.group (Data.List.sort (filter (\x -> length x ==2) intersection))))
-		result = nub $ foldl1 (++) pairs
-		
+    | pairs /= [] = (intersection, result)
+    | otherwise = ([], [])    
+    where
+        possiblePairs = getHiddenPairs row
+        intersection = map (intersect possiblePairs) row
+        pairs = (filter (\x -> length x ==2) (Data.List.group (Data.List.sort (filter (\x -> length x ==2) intersection))))
+        result = nub $ foldl1 (++) pairs
+        
 --removeHiddenPairs row (intersection, pairs)
 removeHiddenPairs :: [Square] -> ([Square],[Square]) -> [Square]
 removeHiddenPairs (x:xs) (_,[]) = (x:xs)
 removeHiddenPairs (x:xs) ([],_) = (x:xs)
 removeHiddenPairs [] _ = []
 removeHiddenPairs (x:xs) ((y:ys), pairs)
-	| y `elem` pairs = y : (removeHiddenPairs xs (ys, pairs))
-	| otherwise = x : (removeHiddenPairs xs (ys, pairs))
+    | y `elem` pairs = y : (removeHiddenPairs xs (ys, pairs))
+    | otherwise = x : (removeHiddenPairs xs (ys, pairs))
 
